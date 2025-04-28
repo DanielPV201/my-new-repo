b0f849dd19cb332e06460eeb2f22c7b38b4c1a31
DanielPV201 <daniel.p.valenciana@gmail.com>
2025-04-28 16:14:58 -0600

    Update app.py
    low dashes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 dfEV = pd.read_csv('Electric_Vehicle_Population_Data.csv')
+dfEV.columns = dfEV.columns.str.replace(' ', '_')
 st.title(":green[E]lectric :green[V]ehicles in :blue[Washington]")
 
 st.write("This dataset contains information about Battery Electric Vehicles (BEVs) and Plug-in Hybrid Electric Vehicles (PHEVs) currently registered with the Washington State Department of Licensing (DOL). The data was obtained from Data.gov, an official website of the United States government")
@@ -30,14 +31,14 @@ st.write("This top ten plot is taken from data from data.wa.gov which is updated
 
 # # # # # # # # # #  HISTPLOT # # # # # # # #
 st.header("EV Brand Trends Over the Last 5 Years", divider=True)
-last_5_years = dfEV[(dfEV['Model Year'] >= 2021) & (dfEV['Model Year'] <= 2025)]
-brand_year_counts = last_5_years.groupby(['Model Year', 'Make']).size().reset_index(name='Vehicle Count')
+last_5_years = dfEV[(dfEV['Model_Year'] >= 2021) & (dfEV['Model_Year'] <= 2025)]
+brand_year_counts = last_5_years.groupby(['Model_Year', 'Make']).size().reset_index(name='Vehicle_Count')
 
 fig = px.line(brand_year_counts, 
-              x='Model Year', 
-              y='Vehicle Count', 
+              x='Model_Year', 
+              y='Vehicle_Count', 
               color='Make',              
-              labels={'Model Year': 'Year', 'Vehicle Count': 'Number of Vehicles'}, 
+              labels={'Model_Year': 'Year', 'Vehicle_Count': 'Number of Vehicles'}, 
               markers=True,
              color_discrete_sequence=px.colors.qualitative.Vivid)
 
@@ -48,14 +49,14 @@ st.write("In this graphic, we can see that Tesla has the fist place in the last 
 # # # # # # # # # # # # # HISTPLOT EXCLUDING TESLA # # # # # # # #
 st.header("EV Brand Trends (Excluding TESLA)", divider=True)
 df_no_tesla = dfEV[dfEV['Make'] != 'TESLA']
-last_5_years_no_tesla = df_no_tesla[(df_no_tesla['Model Year'] >= 2021) & (df_no_tesla['Model Year'] <= 2025)]
-brand_year_counts_no_tesla = last_5_years_no_tesla.groupby(['Model Year', 'Make']).size().reset_index(name='Vehicle Count')
+last_5_years_no_tesla = df_no_tesla[(df_no_tesla['Model_Year'] >= 2021) & (df_no_tesla['Model_Year'] <= 2025)]
+brand_year_counts_no_tesla = last_5_years_no_tesla.groupby(['Model_Year', 'Make']).size().reset_index(name='Vehicle_Count')
 
 fig = px.line(brand_year_counts_no_tesla, 
-              x='Model Year', 
-              y='Vehicle Count', 
+              x='Model_Year', 
+              y='Vehicle_Count', 
               color='Make',              
-              labels={'Model Year': 'Year', 'Vehicle Count': 'Number of Vehicles'}, 
+              labels={'Model_Year': 'Year', 'Vehicle_Count': 'Number of Vehicles'}, 
               markers=True,
              color_discrete_sequence=px.colors.qualitative.Vivid)
 fig.update_layout(xaxis_title="Year", yaxis_title="Number of Vehicles", xaxis=dict(tickmode='linear', tick0=current_year-5, dtick=1))
@@ -66,7 +67,7 @@ st.write("This graph shows how closely contested the battle for second place has
 # # # # # # # # #  NUMBER INPUT # # # # # # #
 st.header("Top 10 Popular EV Brands by Year", divider=True)
 year = st.number_input("Select a Year", min_value=2000, max_value=2025, value=2020)
-df_filtered = dfEV[dfEV['Model Year'] == year]
+df_filtered = dfEV[dfEV['Model_Year'] == year]
 top_10_brands = df_filtered['Make'].value_counts().head(10)
 st.write(f"Top 10 EV Brands in {year}:")
 st.write(top_10_brands)
@@ -76,15 +77,15 @@ st.write(top_10_brands)
 # # # # # # # # SCATTER PLOT # # # # # # # #
 st.header("Electric Range vs Base MSRP", divider="blue")
 st.write("Since we have many zeros and missing values in the Range and MSRP columns, we are going to drop them. Since there are no free cars or cars with zero range, we will assume that these zeros represent missing values.")
-scatter_df = dfEV[(dfEV['Base MSRP'] > 0) & (dfEV['Electric Range'] > 0)]
+scatter_df = dfEV[(dfEV['Base_MSRP'] > 0) & (dfEV['Electric_Range'] > 0)]
 fig = px.scatter(
     scatter_df,
-    x='Base MSRP',
-    y='Electric Range',
+    x='Base_MSRP',
+    y='Electric_Range',
     color='Electric Vehicle Type',
-    hover_data=['Make', 'Model', 'Model Year'],
+    hover_data=['Make', 'Model', 'Model_Year'],
     title='Electric Range vs Base Price (Cleaned Data)',
-    labels={'Base MSRP': 'Base Price ($)', 'Electric Range': 'Range (miles)'},
+    labels={'Base_MSRP': 'Base Price ($)', 'Electric_Range': 'Range (miles)'},
     color_discrete_sequence=px.colors.qualitative.Vivid
 )
 
